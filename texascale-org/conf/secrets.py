0a4234664e0eaafb582386b7002303b4a0a269d6
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-09-04 16:15:08 -0500

    GL-37: Add Texascale templates

---
+++ texascale-org/conf/secrets.py
@@ -0,0 +1,12 @@
+# …
+
+########################
+# DJANGO CMS SETTINGS
+########################
+
+# …
+
+# Overwrite
+_CMS_TEMPLATES.insert(0, ('brand/fullwidth.html', 'Texascale Fullwidth'))
+
+# …
