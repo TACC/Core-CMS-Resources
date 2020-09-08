96352f41f73decebf4c72ed2a63a549b98fbebdf
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-09-08 17:40:56 -0500

    GL-37: Add LCCF assets & config

---
+++ lccf-tacc/conf/secrets.py
@@ -0,0 +1,25 @@
+# …
+
+########################
+# BRANDING.
+
+# …
+
+#_BRANDING = [ _TACC_BRANDING, _UTEXAS_BRANDING ]        # Default TACC Portal.
+_BRANDING = [ _NSF_BRANDING, _TACC_BRANDING, _UTEXAS_BRANDING ]       # NSF Funded TACC Portal.
+
+########################
+# LOGOS.
+
+_LCCF_LOGO = [
+    "lccf",
+    "site_cms/images/org_logos/lccf-white.png",
+    "",
+    "/",
+    "_self",
+    "LCCF Logo",
+    "anonymous",
+    "True"
+]
+
+_LOGO = _LCCF_LOGO
