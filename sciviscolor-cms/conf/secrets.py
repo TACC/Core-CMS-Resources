0a0532bba2a1fb7a87ee64421dbb592eb08f4f60
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-09-08 17:58:01 -0500

    GL-37: Add SCIVIS assets & config

---
+++ sciviscolor-cms/conf/secrets.py
@@ -0,0 +1,17 @@
+# …
+
+########################
+# LOGOS.
+
+_SCIVISCOLOR_LOGO =  [
+    "sciviscolor",
+    "site_cms/images/org_logos/sciviscolor-logo-white.png",
+    "",
+    "/",
+    "_self",
+    "SciVisColor Logo",
+    "anonymous",
+    "True"
+]
+
+_LOGO = _SCIVISCOLOR_LOGO
