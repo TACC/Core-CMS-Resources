d65e18f38a8aebfded466283145812c16128c8a2
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-09-08 17:52:54 -0500

    GL-37: Add TAPIS assets & config

---
+++ tapis-project-org/conf/secrets.py
@@ -0,0 +1,35 @@
+# …
+
+########################
+# BRANDING.
+
+# …
+
+_UHAWAII_BRANDING = [
+    "uhawaii",
+    "site_cms/images/org_logos/hawaii-header-trimmed.png",
+    "branding-uhawaii",
+    "https://www.hawaii.edu/",
+    "_blank",
+    "University of Hawaii Logo",
+    "anonymous",
+    "True"
+]
+
+_BRANDING = [ _NSF_BRANDING, _TACC_BRANDING, _UTEXAS_BRANDING, _UHAWAII_BRANDING ]        # NSF Funded Portal w/ Specific Partners.
+
+########################
+# LOGOS.
+
+_TAPIS_LOGO =  [
+    "tapis",
+    "site_cms/images/org_logos/tapis-logo-navbar.png",
+    "",
+    "/",
+    "_self",
+    "Tapis Logo",
+    "anonymous",
+    "True"
+]
+
+_LOGO = _TAPIS_LOGO
