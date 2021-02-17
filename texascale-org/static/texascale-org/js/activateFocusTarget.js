1b1c7c8921cafa93440741c859bd08c3d0e533dd
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-02-17 11:28:14 -0600

    Noop: Update activateFocusTarget docblock

--- texascale-org/static/texascale-org/js/activateFocusTarget.js
+++ texascale-org/static/texascale-org/js/activateFocusTarget.js
@@ -1,15 +1,26 @@
-/* IMPORTANT: This is required for close button of "Image Map" component, but is an independent re-usable feature */
 /**
- * Activate functionality of elements with `data-focus-target` attribute
+ * Focus on target (by ID) on click of `data-focus-target`-attribute elements
  *
- * On click (or touch) of element:
+ * On click (or touch, or keypress) of element:
  * - If value matches ID of an element, then focus is moved to element with matching ID.
  * - If value is "none" or empty, then page focus is unset.
+ *
+ * WARNING: Do NOT use this on `<a>` (anchor) elements to link to section of a page. Instead use `<a href="#some-heading">Some Heading</a>`.
+ * @example
+ * <!-- Remove hash-based focus -->
+ * <button data-focus-target="none">Close</button>
+ * @example
+ * <!-- Set/Change hash-based focus -->
+ * <button data-focus-target="relevant-field">Skip to "Relevant Field"</button>
+ * ...
+ * <label for="relevant-field">Relevant Field</label>
+ * <input id="relevant-field" ... />
  */
 function activateFocusTarget() {
     const focusTargeterElements = document.querySelectorAll('[data-focus-target]');
 
     focusTargeterElements.forEach( el => {
+        /* TODO: For `<a>` (anchor) elements, abort and log error or warning */
         el.addEventListener('click', ( event ) => {
             const focusTarget = event.currentTarget.dataset.focusTarget;
 
