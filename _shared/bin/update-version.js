a4702ce0bb6a23e9de03aeda2003ee3328f7298f
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-02-07 19:36:10 -0600

    Core-Styles: Part 2: A Bit Simpler Version Script

--- _shared/bin/update-version.js
+++ _shared/bin/update-version.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const { version } = require('../../package.json');
 
 const root = __dirname;
 const inFile = root + '/update-version.css';
 const outFile = root + '/../static/css/src/_version.css';
 
 const input = fs.readFileSync(inFile, 'utf8');
-const output = input.replace(/{{ver}}/g, version);
+const output = input.replace(/{{ver}}/g, process.env.npm_package_version);
 
 fs.writeFileSync(outFile, output, 'utf8');
