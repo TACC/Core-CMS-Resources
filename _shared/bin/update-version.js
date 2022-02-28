521aa7f305920ffbd08e0b07cf91fecb2d610701
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-02-28 15:21:06 -0600

    Core-Styles: Add Git Rev to Version (+ update ver)

--- _shared/bin/update-version.js
+++ _shared/bin/update-version.js
@@ -8,7 +8,39 @@ const root = __dirname;
 const inFile = root + '/update-version.css';
 const outFile = root + '/../static/css/src/_version.css';
 
+const ver = process.env.npm_package_version;
+const rev = getGitRev( getSubmodGitDir() ).substring(0, 7);
+
 const input = fs.readFileSync(inFile, 'utf8');
-const output = input.replace(/{{ver}}/g, process.env.npm_package_version);
+const output = input.replace(/{{ver}}/g, ver).replace(/{{rev}}/g, rev );
+
+/**
+ * Get the Git revision of the current working directory code
+ * @param {string} [gitDir='.git'] - Path to Git directory
+ * @return {string}
+ * @see https://stackoverflow.com/a/34518749/11817077
+ */
+function getGitRev(gitDir='.git') {
+  let rev = fs.readFileSync(gitDir + '/HEAD').toString().trim();
+  const revFile = gitDir + '/' + rev.substring(5);
+
+  if (rev.indexOf(':') !== -1) {
+    console.log('Reading Git revision from: ' + revFile);
+    rev = fs.readFileSync(revFile).toString().trim();
+  }
+
+  console.log('Read Git revision: ' + rev);
+  return rev;
+}
+
+/**
+ * Get the Git directory of the current working directory submodule
+ * @return {string} Path to the Git directory for the submodule
+ */
+function getSubmodGitDir() {
+  const ref = fs.readFileSync('.git').toString().trim();
+
+  return ref.substring(8);
+}
 
 fs.writeFileSync(outFile, output, 'utf8');
