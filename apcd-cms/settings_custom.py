4911af40da45490e34f4e3686e487e19014f055a
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-05-11 11:40:11 -0500

    APCD cms resources

---
+++ apcd-cms/settings_custom.py
@@ -0,0 +1,37 @@
+# CUSTOM SETTINGS VALUES.
+# TACC WMA CMS SITE:
+# *.APCD.TACC.UTEXAS.EDU
+
+
+########################
+# DJANGO CMS SETTINGS
+########################
+
+CMS_TEMPLATES = (
+    ('apcd-cms/templates/standard.html', 'Standard'),
+    ('apcd-cms/templates/fullwidth.html', 'Full Width'),
+    ('guide.html', 'Guide'),
+    ('guides/getting_started.html', 'Guide: Getting Started'),
+    ('guides/data_transfer.html', 'Guide: Data Transfer'),
+    ('guides/data_transfer.globus.html', 'Guide: Globus Data Transfer'),
+    ('guides/portal_technology.html', 'Guide: Portal Technology Stack')
+)
+
+########################
+# TACC: LOGOS
+########################
+
+LOGO = [
+    "tacc",
+    "apcd-cms/img/org_logos/tacc-white.png",
+    "",
+    "/",
+    "_self",
+    "APCD: All Player Claims Database",
+    "anonymous",
+    "True"
+]
+
+FAVICON = {
+    "img_file_src": "apcd-cms/img/org_logos/favicon.ico"
+}
