79a005d45d8b9b6deb06f1da6c6884859ecb5536
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-05-23 10:29:07 -0500

    chore(fp-1652): utrc: cleanup template deprecation

--- utrc-cms/settings_custom.py
+++ utrc-cms/settings_custom.py
@@ -21,11 +21,11 @@ CMS_TEMPLATES = (
     ('guides/portal_technology.html', 'Guide: Portal Technology Stack')
 )
 
-# FP-1645: Remove CMS_TEMPLATES_DEPRECATED
-CMS_TEMPLATES_DEPRECATED = (
+# Deprecated Templates
+# TODO: FP-1645: Remove
+CMS_TEMPLATES += (
     ('utrc-cms/templates/home.html', 'DEPRECATED Home'),
 )
-CMS_TEMPLATES += CMS_TEMPLATES_DEPRECATED
 
 ########################
 # TACC: LOGOS
