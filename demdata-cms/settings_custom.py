f5ae5d35eed958aec258da540faf3696f99fced5
John Gentle <taoteg@gmail.com>
2023-01-27 14:11:39 -0600

    Update settings_custom.py
    Added branding code.

--- demdata-cms/settings_custom.py
+++ demdata-cms/settings_custom.py
@@ -54,6 +54,60 @@ CMS_TEMPLATES = (
 )
 
 ########################
+# TACC: BRANDING
+########################
+
+# LOOK INTO THIS SOLUTION.
+# from taccsite_cms.settings import TACC_BRANDING, UTEXAS_BRANDING, NSF_BRANDING
+
+TACC_BRANDING = [
+    "tacc",
+    "epoc-cms/img/org_logos/tacc-white.png",
+    "branding-tacc",
+    "https://www.tacc.utexas.edu/",
+    "_blank",
+    "TACC Logo",
+    "anonymous",
+    "True"
+]
+
+UTEXAS_BRANDING = [
+    "utexas",
+    "epoc-cms/img/org_logos/utaustin-white.png",
+    "branding-utaustin",
+    "https://www.utexas.edu/",
+    "_blank",
+    "University of Texas at Austin Logo",
+    "anonymous",
+    "True"
+]
+
+# NSF_BRANDING = [
+#     "nsf",
+#     "epoc-cms/img/org_logos/nsf-white.png",
+#     "branding-nsf",
+#     "https://www.nsf.gov/",
+#     "_blank",
+#     "NSF Logo",
+#     "anonymous",
+#     "True"
+# ]
+
+# CUSTOM_BRANDING = [
+#     "epoc",
+#     "epoc-cms/img/org_logos/esnet-white-logo.png",
+#     "branding-logo--short",
+#     "https://www.es.net/",
+#     "_blank",
+#     "ESnet Logo",
+#     "anonymous",
+#     "True",
+# ]
+
+# BRANDING = [ TACC_BRANDING, UTEXAS_BRANDING, CUSTOM_BRANDING ]
+BRANDING = [ TACC_BRANDING, UTEXAS_BRANDING ]
+
+########################
 # TACC: LOGOS
 ########################
 
