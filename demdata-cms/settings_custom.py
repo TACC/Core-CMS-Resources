10d8ab732921a1e6b90e754311df8f35d0b435d4
John Gentle <taoteg@gmail.com>
2023-01-27 14:10:15 -0600

    Update settings_custom.py
    Added navigation changes to UI in DemData settings_custom.py.

--- demdata-cms/settings_custom.py
+++ demdata-cms/settings_custom.py
@@ -1,7 +1,43 @@
 # CUSTOM SETTINGS VALUES.
 # TACC WMA CMS SITE:
-# *.democemocratizing-site.TACC.UTEXAS.EDU
+# *.DEMOCTRATIZING-SITE.TACC.UTEXAS.EDU
 
+# CUSTOM SETTINGS VALUES.
+# TACC WMA CMS SITE:
+# _*.EPOC.TACC.UTEXAS.EDU
+# https:///epoc.global
+
+'''
+A `settings_custom.py` file can override default values in `settings.py`.
+The file is loaded after default settings but before settings assignment is complete, so we can override settings in:
+- _either_ `settings_custom.py` (usually set in custom sites)
+- _or_ in `settings_local.py` (usually used in a local dev environment)
+To override a setting:
+1. Copy/Paste the default settings.
+2. Set the new/custom values.
+If a setting override is for a custom CMS Project, the change is made in the `settings_custom.py` file of that project's directory and (if not a secret) can be committed.
+Unless modifying default behavior for the CMS Core (and thus all custom Projects) (which probably means that setting should be in the `settings.py` file), the `settings_custom.py` file that should be modified is the one in the appropriate CMS Project directory.
+'''
+
+########################
+# WALKTHROUGH
+########################
+
+# To change LDAP auth settings for a custom CMS Project (e.g. `epoc-cms`):
+# 1. Copy the setting from `settings.py`
+# 2. Assign the new value in `Core-CMS/taccsite_custom/epoc-cms/settings_custom.py`.
+AUTH_LDAP_SERVER_URI = "ldap://cluster.ldap.tacc.utexas.edu"
+
+# The same goes for other more commonly customized values like below.
+
+########################
+# TACC: PORTAL
+########################
+
+# Does this CMS site have a portal (default value: True)?
+INCLUDES_CORE_PORTAL = False 
+INCLUDES_PORTAL_NAV = False
+INCLUDES_SEARCH_BAR = False
 
 ########################
 # DJANGO CMS SETTINGS
