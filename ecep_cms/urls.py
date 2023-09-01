2f62475fdda29ba674850fd47d119adeef7fbabe
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-09-01 16:25:02 -0500

    chore: delete ecep_cms
    It is migrated to TACC/Core-CMS-Custom.

--- ecep_cms/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from taccsite_cms.urls import *
-
-from django.conf.urls import include, url
-
-urlpatterns += [
-    # Support `taggit_autosuggest` (from `djangocms-blog`)
-    url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
-]
